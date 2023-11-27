f7bb3614f6a9287a115c3aedda00d990ac0599f0
hedys9 <hedysadeghi@gmail.com>
2023-11-27 11:26:14 +0100

    discount

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
+let arrayDiscountCodes=["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+let discount=false;
+
 
 
 
 //-------FUNZIONI-----------
+
+function controlDiscount(discountInput){
+
+   for (i=0; i<arrayDiscountCodes.length; i++){
+        if (discountInput== arrayDiscountCodes[i]);
+        discount=true;
+        break;
+    };
+    return discount;
+
+}
+
+
 function calculateOffer(event){
     event.preventDefault();
 
@@ -12,24 +28,53 @@ function calculateOffer(event){
     let typeOfWork=document.getElementById("type-of-work");  
     let typeOfWorkSelected = typeOfWork.options[typeOfWork.selectedIndex].value;
 
-
     let finalPrice=0;
+    let discountInput=document.getElementById("discount-code").value;
+    let discount= controlDiscount(discountInput);
 
-    if (typeOfWorkSelected=="1"){
-        finalPrice+= hours *20.50;
-        finalPrice= finalPrice.toFixed(2);
-        console.log(finalPrice);
-    }
-    else if (typeOfWorkSelected=="2"){
-        finalPrice+= hours *15.3;
-        finalPrice= finalPrice.toFixed(2);
-        console.log(finalPrice);
+    if (discount){
+        if (typeOfWorkSelected=="1"){
+            finalPrice+= hours *20.50;
+            finalPrice*= 0.75;
+            finalPrice= finalPrice.toFixed(2);
+            console.log(finalPrice);
+        }
+        else if (typeOfWorkSelected=="2"){
+            finalPrice+= hours *15.3;
+            finalPrice*= 0.75;
+            finalPrice= finalPrice.toFixed(2);
+            console.log(finalPrice);
+    
+        }
+        else if (typeOfWorkSelected=="3"){
+            finalPrice+= hours *33.6;
+            finalPrice*= 0.75;
+            finalPrice= finalPrice.toFixed(2);
+            console.log(finalPrice);
+        }
 
     }
-    else if (typeOfWorkSelected=="3"){
-        finalPrice+= hours *33.6;
-        finalPrice= finalPrice.toFixed(2);
-        console.log(finalPrice);
+
+    else{
+        alert("discount non valido");
+
+        if (typeOfWorkSelected=="1"){
+            finalPrice+= hours *20.50;
+            finalPrice= finalPrice.toFixed(2);
+            console.log(finalPrice);
+        }
+        else if (typeOfWorkSelected=="2"){
+            finalPrice+= hours *15.3;
+            finalPrice= finalPrice.toFixed(2);
+            console.log(finalPrice);
+    
+        }
+        else if (typeOfWorkSelected=="3"){
+            finalPrice+= hours *33.6;
+            finalPrice= finalPrice.toFixed(2);
+            console.log(finalPrice);
+    
+        }
 
     }
 
